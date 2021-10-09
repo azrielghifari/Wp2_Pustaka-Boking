1a7939d73a574e194248d1fa18dde16f8684e932
azrielghifari <azrielghifari360@gmail.com>
2021-10-09 21:07:32 +0700

    menambahkan new file

---
+++ application/views/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Muhammad Azriel Ghifari
+              Saya tinggal di daerah Matraman
+              olahraga yang saya sukai adalah
+              Futsal";
+    }
+}
